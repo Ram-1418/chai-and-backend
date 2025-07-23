79113b7bc824085a5176a936db7595483b1d98b8
Ram <rameshwarpatil9689@gmail.com>
2025-07-23 05:57:31 +0000

    feat: set up MongoDB connection and update project dependencies

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app=express()
+
+// ( async()=>{
+// try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     application.on("error",(error)=>{
+//         console.log("ERRR:",error);
+//         throw  error
+        
+//     })
+//     app.listtn(process.env.PORT,()=>{
+//         console.log(`App is listing on port${process.env.PORT}`)
+        
+//     })
+// } catch (error) {
+//     console.log("ERRR:",error)
+// }
+// })()
