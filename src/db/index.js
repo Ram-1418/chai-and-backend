d0f41997501a6243e1bef9cb1e6150a8b27a91e8
Rameshwar Patil <23521516.acs@dypvp.edu.in>
2025-07-30 06:15:36 +0000

    feat: implement user registration route and connect user controller & All setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,14 @@ import { DB_NAME } from "../constants.js";
 
 
 const connectDB=async()=>{
+    console.log(process.env.MONGODB_URL);
+    
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} `)
     } catch (error) {
+        console.dir(error);
+        
         console.log("MONGODB CONNECTION ERROR",error)
         process.exit(1)
     }
