5ea91d585588437b533d9897fc54d9a4baefcdac
Ram <rameshwarpatil9689@gmail.com>
2025-09-16 09:21:53 +0000

    changes in route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import {refreshAcessToken, registerUser} from "../controllers/user.controller.js"
+import {refreshAcessToken, registerUser,loginUser,logoutUser} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { loginUser } from "../controllers/user.controller.js";
-import { logoutUser } from "../controllers/user.controller.js";
+
 const router = Router()
 
 router.route("/register").post(
